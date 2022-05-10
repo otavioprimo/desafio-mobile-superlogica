f8ffcf7fbf3966035a4c6e1a211e797a988c07fa
Fernandinho Mac <fernando.khoury@hotmail.com>
2022-05-10 15:02:34 -0300

    Imagem png

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,20 +1,46 @@
 import React from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { TouchableHighlight } from "react-native-gesture-handler";
 
 interface Props {
   data: any;
   setModalVisible: any;
 }
 
+// const Portal = ({children}) => {
+//   return (
+//     <View
+//       style={styles.portal}
+//     >
+//       <Image
+//         source={require('')}
+//         style={styles.image}
+//       />
+//       {children}
+//     </View>
+//   )
+// }
+
 export const Card = ({ data, setModalVisible }: Props) => {
   const { item } = data;
 
   return (
     <TouchableOpacity style={styles.container} onPress={() => setModalVisible(true)}>
-      <Image 
-        source={{ uri: item.image }} 
-        style={styles.image} 
-      />
+      <TouchableOpacity
+        style={[styles.profileImgContainer, { borderColor: 'white', borderWidth: 1 }]}
+      >
+        <Image
+          source={{ uri: item.image }} 
+          style={styles.image} 
+        />
+        <View style={styles.portal}>
+          <Image
+            source={require('./portal.png')} 
+            style={styles.overImage} 
+            resizeMode={'cover'}
+          />
+        </View>
+      </TouchableOpacity>
       <View style={styles.cardText}>
         <Text>{item.name}</Text>
         <Text>{item.species}</Text>
@@ -40,7 +66,25 @@ const styles = StyleSheet.create({
     margin: 8,
     borderRadius: 24,
   },
+  overImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 24,
+  },
   cardText: {
     justifyContent: "space-between",
+    marginLeft: 16
   },
+  profileImgContainer: {
+    borderRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  portal: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  }
 });
