535549617db4a2cffe3db62df27bae88e4045d7a
Fernando Khoury Yazbek <fernando.khoury@hotmail.com>
2022-05-10 15:59:58 -0300

    Imagem png

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { TouchableHighlight } from "react-native-gesture-handler";
 
 interface Props {
-  data: any;
-  setModalVisible: any;
+	data: any;
+	setModalVisible: any;
+}
+
+const Portal = ({image}) => {
+	return (
+		<TouchableOpacity
+			style={[imageStyles.container]}
+		>
+			<Image
+				source={{ uri: image }} 
+				style={styles.image} 
+			/>
+			<View style={imageStyles.portal}>
+				<Image
+					source={require('./portal.png')} 
+					style={imageStyles.overImage} 
+					resizeMode={'cover'}
+				/>
+			</View>
+		</TouchableOpacity>
+	)
 }
 
 // const Portal = ({children}) => {
@@ -22,69 +41,61 @@ interface Props {
 // }
 
 export const Card = ({ data, setModalVisible }: Props) => {
-  const { item } = data;
+	const { item } = data;
 
-  return (
-    <TouchableOpacity style={styles.container} onPress={() => setModalVisible(true)}>
-      <TouchableOpacity
-        style={[styles.profileImgContainer, { borderColor: 'white', borderWidth: 1 }]}
-      >
-        <Image
-          source={{ uri: item.image }} 
-          style={styles.image} 
-        />
-        <View style={styles.portal}>
-          <Image
-            source={require('./portal.png')} 
-            style={styles.overImage} 
-            resizeMode={'cover'}
-          />
-        </View>
-      </TouchableOpacity>
-      <View style={styles.cardText}>
-        <Text>{item.name}</Text>
-        <Text>{item.species}</Text>
-        <Text>{item.status}</Text>
-      </View>
-    </TouchableOpacity>
-  );
+	return (
+		<TouchableOpacity style={styles.container} onPress={() => setModalVisible(true)}>
+			<Portal 
+				image={item.image}
+			/>
+			<View style={styles.cardText}>
+				<Text style={styles.text}>{item.name}</Text>
+				<Text style={styles.text}>{item.species}</Text>
+				<Text style={styles.text}>{item.status}</Text>
+			</View>
+		</TouchableOpacity>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "row",
+	container: {
+		flex: 1,
+		flexDirection: "row",
 
-    margin: 16,
-    padding: 8,
-    borderRadius: 8,
-    backgroundColor: "#2F9331", // #2F9331 / #8BCF21
-  },
-  image: {
-    width: 50,
-    height: 50,
-    margin: 8,
-    borderRadius: 24,
-  },
-  overImage: {
-    width: 60,
-    height: 60,
-    borderRadius: 24,
-  },
-  cardText: {
-    justifyContent: "space-between",
-    marginLeft: 16
-  },
-  profileImgContainer: {
-    borderRadius: 40,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  portal: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  }
+		margin: 16,
+		padding: 8,
+		borderRadius: 8,
+		backgroundColor: "#2F9331", // #2F9331 / #8BCF21
+	},
+	image: {
+		width: 50,
+		height: 50,
+		margin: 8,
+		borderRadius: 24,
+	},
+	cardText: {
+		justifyContent: "space-between",
+		marginLeft: 16
+	},
+	text: {
+		color: '#071B09',
+		fontWeight: 'bold'
+	},
 });
+
+const imageStyles = StyleSheet.create({
+	container: {
+		borderRadius: 40,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	portal: {
+		position: 'absolute',
+		justifyContent: 'center',
+	},
+	overImage: {
+		width: 84,
+		height: 84,
+		borderRadius: 24,
+	},
+})
